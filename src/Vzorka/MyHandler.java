a5aeea3aa243bad6ac02947c42a7d99f158e6c0a
Dokonaly <petko.hamar@gmail.com>
2014-09-29 15:54:36 +0200

    Vytvorenie parsovania country
    doplnenie regexov - 12 roznych premennych

--- src/Vzorka/MyHandler.java
+++ src/Vzorka/MyHandler.java
@@ -38,34 +38,164 @@ public class MyHandler  extends DefaultHandler {
         } 
     }
  
+    public String PouziRegex(String regex, String vstup){
+    	Pattern pattern = Pattern.compile(regex);
+    	Matcher matcher = pattern.matcher(vstup);
+    	if (matcher.find())
+    	{
+    		return matcher.group(0);
+    	}
+    	return null;
+    }
+    
+    public boolean oparsujCountry(boolean flag, String vysledok){
+    	
+    	//country
+    	String vystup = PouziRegex("\\| ?conventional_long_name = [A-Za-z0-9 _ =*.:?!()+-<>\\]\\[#@\\{}'`$%^&;<>,ֹציז]+ ", vysledok);  	
+    	if (vystup != null){
+    		//System.out.print(vystup);
+    	    infobox.setTitle(vystup);
+     	    flag = true;
+    	}
+    
+    	vystup = PouziRegex("\\| ?common_name = [A-Za-z0-9 _ =*.:?!()+-<>\\]\\[#@\\{}'`$%^&;<>,ֹציז]+ ", vysledok);  	
+    	if (vystup != null){
+    		//System.out.print(vystup);
+    	    infobox.setCommon_name(vystup);
+     	    flag = true;
+    	}
+    	
+    	vystup = PouziRegex("\\| ?image_flag = [A-Za-z0-9 _ =*.:?!()+-<>\\]\\[#@\\{}'`$%^&;<>,ֹציז]+ ", vysledok);  	
+    	if (vystup != null){
+    		//System.out.print(vystup);
+    	    infobox.setImage_flag(vystup);
+     	    flag = true;
+    	}
+    	
+    	vystup = PouziRegex("\\| ?image_coat = [A-Za-z0-9 _ =*.:?!()+-<>\\]\\[#@\\{}'`$%^&;<>,ֹציז]+ ", vysledok);  	
+    	if (vystup != null){
+    		//System.out.print(vystup);
+    	    infobox.setImage_coat(vystup);
+     	    flag = true;
+    	}
+    	
+    	vystup = PouziRegex("\\| ?capital = [A-Za-z0-9 _ =*.:?!()+-<>\\]\\[#@\\{}'`$%^&;<>,ֹציז]+ ", vysledok);  	
+    	if (vystup != null){
+    		//System.out.print(vystup);
+    	    infobox.setCapital(vystup);
+     	    flag = true;
+    	}
+    	
+    	vystup = PouziRegex("\\| ?official_religion = [A-Za-z0-9 _ =*.:?!()+-<>\\]\\[#@\\{}'`$%^&;<>,ֹציז]+ ", vysledok);  	
+    	if (vystup != null){
+    		//System.out.print(vystup);
+    	    infobox.setOfficial_religion(vystup);
+     	    flag = true;
+    	}
+    	
+    	vystup = PouziRegex("\\| ?official_languages = [A-Za-z0-9 _ =*.:?!()+-<>\\}\\[#@\\{|'`$%^&;<>,ֹציז]+ ", vysledok);  	
+    	if (vystup != null){
+    		//System.out.print(vystup);
+    	    infobox.setOfficial_languages(vystup);
+     	    flag = true;
+    	}
+    	
+    	vystup = PouziRegex("\\| ?government_type = [A-Za-z0-9 _ =*.:?!()+-<>\\]\\[ #@\\{}'`$%^&;<>,ֹציז]+ ", vysledok);  	
+    	if (vystup != null){
+    		//System.out.print(vystup);
+    	    infobox.setGovernment_type(vystup);
+     	    flag = true;
+    	}
+    	
+    	vystup = PouziRegex("\\| ?area_km2 = [A-Za-z0-9 _ =*.:?!()+-<>\\]\\[#@\\{}'`$%^&;<>,ֹציז]+ ", vysledok);  	
+    	if (vystup != null){
+    		//System.out.print(vystup);
+    	    infobox.setArea_km2(vystup);
+     	    flag = true;
+    	}
+    	
+    	vystup = PouziRegex("\\| ?area_sq_mi = [A-Za-z0-9 _ =*.:?!()+-<>\\]\\[#@\\{}'`$%^&;<>,ֹציז]+ ", vysledok);  	
+    	if (vystup != null){
+    		//System.out.print(vystup);
+    	    infobox.setArea_sq_mi(vystup);
+     	    flag = true;
+    	}
+    	
+    	vystup = PouziRegex("\\| ?population_estimate = [A-Za-z0-9 _ =*.:?!()+->\\]\\[#@\\{}'`$%^&;<>,ֹציז]+ ", vysledok);  	
+    	if (vystup != null){
+    		//System.out.print(vystup);
+    	    infobox.setPopulation_estimate(vystup);
+     	    flag = true;
+    	}
+    	
+    	vystup = PouziRegex("\\| ?population_estimate_rank = [A-Za-z0-9 _ =*.:?!()+-<>\\]\\[#@\\{}'`$%^&;<>,ֹציז]+ ", vysledok);  	
+    	if (vystup != null){
+    		//System.out.print(vystup);
+    	    infobox.setPopulation_estimate_rank(vystup);
+     	    flag = true;
+    	}
+    	
+    	vystup = PouziRegex("\\| ?currency_code = [A-Za-z0-9 _ =*.:?!()+-<>\\]\\[#@\\{}'`$%^&;<>,ֹציז]+ ", vysledok);  	
+    	if (vystup != null){
+    		//System.out.print(vystup);
+    	    infobox.setCurrency_code(vystup);
+     	    flag = true;
+    	}
+    	
+    	vystup = PouziRegex("\\| ?currency = [A-Za-z0-9 _ =*.:?!()+-<>\\]\\[#@\\{}'`$%^&;<>,ֹציז]+ ", vysledok);  	
+    	if (vystup != null){
+    		//System.out.print(vystup);
+    	    infobox.setCurrency(vystup);
+     	    flag = true;
+    	}
+    	
+    	return flag;
+    }
+    
+    
     @Override
     public void endElement(String uri, String localName, String qName) throws SAXException {
+    	boolean flag_country = false;
+    	
         if (bTitle) {
           
         	String vysledok = sb.toString();
         	vysledok = vysledok.replaceAll("(\r\n|\n)", " "); //zarovnanie do jedneho riadku
         	vysledok = vysledok.trim().replaceAll(" +", " "); //odstranenie nepotrebnych medzier
-        	Pattern pattern = Pattern.compile(
-        			"\\{\\{Infobox disease [A-Za-z0-9 |_ =*.:?!()+-<>\\]\\[#@\\{'`$%^&;<>,ֹציז]+ " +
-        			"| \\{\\{Infobox person [A-Za-z0-9 |_ =*.:?!()+-<>\\]\\[#@\\{'`$%^&;<>,ֹציז]+ " +
-        			"| \\{\\{Infobox book [A-Za-z0-9 |_ =*.:?!()+-<>\\]\\[#@\\{'`$%^&;<>,ֹציז]+" +
-        			"| \\{\\{Infobox award [A-Za-z0-9 |_ =*.:?!()+-<>\\]\\[#@\\{'`$%^&;<>,ֹציז]+ "			
-        			); //}
-        	Matcher matcher = pattern.matcher(vysledok);
-        	if (matcher.find())
-        	{
-        	    System.out.println(matcher.group(0));
-        	    //infobox.setTitle(vysledok);
-        	    infobox.setTitle(matcher.group(0));
-         	    infoboxList.add(infobox);
+        	
+        	String text = PouziRegex("\\{\\{Infobox country \\s*(.*)", vysledok);
+        	
+        	
+        	if (text !=null){
+        	flag_country  = oparsujCountry(flag_country, text);
         	}
-        	
+        	if (flag_country == true){
+        		if ( infobox.getTitle()!= null ||  infobox.getCommon_name() != null){
+        			System.out.println(infobox.getTitle()+" "
+        		
+        					+infobox.getCommon_name() +" "
+        					+infobox.getImage_flag()+" "
+        					+infobox.getImage_coat()+" "
+        					+infobox.getCapital()+" "
+        					+infobox.getOfficial_religion()+" "
+        					+infobox.getOfficial_languages()+" "
+        					+infobox.getGovernment_type()+" "
+        					+infobox.getArea_km2()+" "
+        					+infobox.getArea_sq_mi()+" "
+        					+infobox.getCurrency()+" "
+        					+infobox.getCurrency_code()+" "
+        					+infobox.getPopulation_estimate()+" "
+        					+infobox.getPopulation_estimate_rank()
+        					);
+        			
+        			infoboxList.add(infobox);
+            	       			
+        		}
+        	}	
+
             bTitle = false;
-        	
-        	 
+            flag_country = false;     	 
         }
-       
-       
     }
  
     @Override
--- src/Vzorka/Parser.java
+++ src/Vzorka/Parser.java
@@ -26,7 +26,22 @@ public class Parser {
 	        List<Infobox> InfoboxList = handler.getInfoboxList();
 	        
 	        for(Infobox inf : InfoboxList){
-	        	out.println(inf.getTitle());
+	        	out.println(inf.getTitle()+" "
+        		
+        					+inf.getCommon_name() +" "
+        					+inf.getImage_flag()+" "
+        					+inf.getImage_coat()+" "
+        					+inf.getCapital()+" "
+        					+inf.getOfficial_religion()+" "
+        					+inf.getOfficial_languages()+" "
+        					+inf.getGovernment_type()+" "
+        					+inf.getArea_km2()+" "
+        					+inf.getArea_sq_mi()+" "
+        					+inf.getCurrency()+" "
+        					+inf.getCurrency_code()+" "
+        					+inf.getPopulation_estimate()+" "
+        					+inf.getPopulation_estimate_rank()
+        					);
 	        	//System.out.println(inf.getTitle());
 	        }
 	        
